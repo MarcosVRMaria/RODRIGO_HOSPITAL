882a385df4f810245bf340927688fbc91f8ee809
MarcosVRMaria <marcosviniciusbatata@gmail.com>
2024-05-16 21:07:00 -0300

    AAAAAAAAAAAAA

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,84 +1,111 @@
 import React, { useEffect, useState } from 'react';
 import moment from "moment";
-
+import "./App.css"
 
 function App() {
-  const [lista, setLista] = useState([]);
-  const listaLocal = [];
+  const [normais, setNormais] = useState([]);
+  const [prioritarios, setPrioritarios] = useState([]);
+  const [emergencias, setEmergencias] = useState([]);
 
-  let p = 1;
-  let e = 1;
-  let n = 1;
+  const [normalCounter, setNormalCounter] = useState(1);
+  const [prioritarioCounter, setPrioritarioCounter] = useState(1);
+  const [emergenciaCounter, setEmergenciaCounter] = useState(1);
 
-  const normal = (n) => {
-
+  const normal = () => {
     let enviar = {
-      Senha: {
-        senha: "N" + (n+1),
-        tipoDeAtendimento: "Normal",
-        hora: moment().format('LT')
-      }
+      senha: "N" + normalCounter,
+      tipoDeAtendimento: "Normal",
+      hora: moment().format('LT')
     };
-
-    listaLocal.push(enviar);
-    setLista([...lista, enviar]);
+    setNormais(prevLista => [...prevLista, enviar]);
+    setNormalCounter(prevCounter => prevCounter + 1);
   };
 
   const prioritario = () => {
-
     let enviar = {
-      Senha: {
-        senha: "P" + p++,
-        tipoDeAtendimento: "Prioritário",
-        hora: moment().format('LT')
-      }
+      senha: "P" + prioritarioCounter,
+      tipoDeAtendimento: "Prioritário",
+      hora: moment().format('LT')
     };
-    listaLocal.push(enviar);
-    setLista([...lista, enviar]);
+    setPrioritarios(prevLista => [...prevLista, enviar]);
+    setPrioritarioCounter(prevCounter => prevCounter + 1);
   };
 
   const emergencia = () => {
-
     let enviar = {
-      Senha: {
-        senha: "E" + e++,
-        tipoDeAtendimento: "Emergência",
-        hora: moment().format('LT')
-      }
+      senha: "E" + emergenciaCounter,
+      tipoDeAtendimento: "Emergência",
+      hora: moment().format('LT')
     };
-    listaLocal.push(enviar);
-    setLista([...lista, enviar]);
+    setEmergencias(prevLista => [...prevLista, enviar]);
+    setEmergenciaCounter(prevCounter => prevCounter + 1);
   };
 
-  const listaData = Object.values(lista).map(lista => ({
-    senha: lista.Senha.senha,
-    tipo: lista.Senha.tipoDeAtendimento,
-    hora: lista.Senha.hora
-  }));
-  
+  const mergedList = () => {
+    const result = [];
+
+    // Add all emergências first
+    emergencias.forEach(item => result.push(item));
+
+    // Merge prioritários and normais
+    let normalIndex = 0;
+    let prioritarioIndex = 0;
+    let canAddPrioritario = true;
+
+    while (normalIndex < normais.length || prioritarioIndex < prioritarios.length) {
+      if (prioritarioIndex < prioritarios.length && canAddPrioritario) {
+        result.push(prioritarios[prioritarioIndex++]);
+        canAddPrioritario = false;
+      }
+
+      if (normalIndex < normais.length) {
+        result.push(normais[normalIndex++]);
+        canAddPrioritario = true; // Allow one prioritário after a normal
+      } else if (!canAddPrioritario && prioritarioIndex < prioritarios.length) {
+        result.push(prioritarios[prioritarioIndex++]);
+        canAddPrioritario = false;
+      }
+    }
+
+    return result;
+  };
+
+  const listaData = mergedList();
+
   useEffect(() => {
-    // console.log(lista)
-    console.log(listaData)
-  }, [listaData]);
+    console.log(listaData);
+  }, [normais, prioritarios, emergencias]);
 
   return (
-    <>
-      <h1>GESTÃO DE SENHAS HOSP. SENAI</h1>
-      <h2>Selecione conforme a necessidade</h2>
-      <button onClick={normal}>Normal</button>
-      <button onClick={emergencia}>Emergencia</button>
-      <button onClick={prioritario}>Prioridade</button>
-      <ul>
-        {listaData.map((i) => (
-          <div><br></br>
-            <span>{i.senha}</span>
-            <span>{i.tipo}</span>
-            <span>{i.hora}</span>
-          </div>
-        ))}
-      </ul>
-    </>
-  );
+    <div className='content'>
+    <h1>GESTÃO DE SENHAS HOSP. SENAI</h1>
+    <div className='base-content'>
+    <h2>Selecione conforme a necessidade</h2>
+    <div className='bnt-content'>
+    <button className='normal' onClick={normal}>Normal</button>
+    <button className='emergencia' onClick={emergencia}>Emergencia</button>
+    <button className='prioritario' onClick={prioritario}>Prioridade</button>
+    </div>
+
+    <ul>
+      <div className='text-content'>
+      <h2 className='text-senha'>SENHA</h2>
+      <h2 className='text-tipo'>TIPO DE ATENDIMENTO</h2>
+      <h2 className='text-horario'>HORÁRIO DE ATENDIMENTO</h2>
+      </div>
+      {listaData.map((i, index) => (
+        <div key={index} className='ficha'>
+          <br />
+          
+          <span>{i.senha}</span><br />
+          <span>{i.tipoDeAtendimento}</span><br />
+          <span>{i.hora}</span>
+        </div>
+      ))}
+    </ul>
+    </div>
+    </div>
+);
 }
 
 export default App;
